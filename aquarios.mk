69770e1ca506ef3f20fee5f5f0eacb4717f63494
calebcabob <calphonic@gmail.com>
2020-04-10 16:56:11 -0400

    Update fingerprint to April 2020

--- aquarios.mk
+++ aquarios.mk
@@ -38,12 +38,12 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="blueline" \
     TARGET_DEVICE="blueline" \
-    PRIVATE_BUILD_DESC="blueline-user 10 QQ2A.200305.003 6138846 release-keys"
+    PRIVATE_BUILD_DESC="blueline-user 10 QQ2A.200405.005 6254899 release-keys"
 
-BUILD_FINGERPRINT := "google/blueline/blueline:10/QQ2A.200305.003/6138846:user/release-keys"
+BUILD_FINGERPRINT := "google/blueline/blueline:10/200405.005/6254899:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/blueline/blueline:10/QQ2A.200305.003/6138846:user/release-keys
+    ro.build.fingerprint=google/blueline/blueline:10/QQ2A.200405.005/6254899:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/blueline/vendor.img
 
@@ -53,3 +53,6 @@ $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 # Boot animation properties
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2160
+
+PRODUCT_PACKAGES += \
+    GoogleCamera
